3b7e822c37b76bea1da20c06b995d08fdfdb858c
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-01 22:00:56 +0300

    :sparkles: (app/blocks/slider) Rewrite markup, add js for tab-desk

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -8,45 +8,104 @@ export default window.addEventListener(`DOMContentLoaded`, () => {
   const mqlDesktop = window.matchMedia(`only screen and (min-width: 768px)`);
 
   // sliders
-  const parentSlider = document.querySelector(`[data-js='slider'].slider--parent`);
-  const childSlidersCollection = utils.$$(`[data-js='slider'].slider--child`);
+  const parentSlider = document.querySelector(`[data-js='slider--parent']`);
+  const childSlidersCollection = utils.$$(`[data-js='slider--child']`);
+
+  // slides
+  const slidesCollection = utils.$$(`[data-js='slider--child'] [data-js='slide']`);
+  const slidesWrappersCollection = utils.$$(`.slider__wrapper`);
 
   // Flickity' instances
   const parentFlkty = {
-    cellSelector: `.slide--parent`,
+    cellSelector: `.slider__cell--parent`,
     prevNextButtons: true,
     pageDots: false,
     wrapAround: true
   };
 
-  const childFlkty = {
-    cellSelector: `.slide--child`,
+  const childFlktyForMobile = {
+    cellSelector: `.slider__cell--child`,
     prevNextButtons: true,
     pageDots: false,
     groupCells: 3,
     wrapAround: true
   };
 
+  const childFlktyForDesktop = {
+    cellSelector: `.slider__cell--child`,
+    prevNextButtons: false,
+    pageDots: true,
+    wrapAround: true
+  };
+
   let flkty = [];
 
+  // Enable sliders by default
   /*eslint-disable */
   new Flickity(parentSlider, parentFlkty);
   /*eslint-enable */
 
+  childSlidersCollection.forEach((slider, index) => {
+    flkty[index] = new Flickity(slider, childFlktyForMobile);
+  });
+
   /**
-  * Enable / disable inner slider for different viewports
-  */
-  const matchMedia = () => {
+   * Conditionally assign inner slider' properties
+   */
+  const transformInnerSlider = () => {
     childSlidersCollection.forEach((slider, index) => {
       if (mqlMobile.matches) {
-        flkty[index] = new Flickity(slider, childFlkty);
+        flkty[index].destroy();
+        flkty[index] = new Flickity(slider, childFlktyForMobile);
       }
       if (mqlDesktop.matches) {
         if (flkty[index]) {
           flkty[index].destroy();
+          flkty[index] = new Flickity(slider, childFlktyForDesktop);
         }
       }
     });
+  };
+
+  /**
+ * Conditionally change inner slider' cell
+ */
+  const changeSliderCell = () => {
+    // slides
+    slidesCollection.forEach((slide) => {
+      if (mqlMobile.matches) {
+        if (!slide.classList.contains(`slider__cell`)) {
+          slide.classList.add(`slider__cell`, `slider__cell--child`);
+        }
+      }
+      if (mqlDesktop.matches) {
+        if (slide.classList.contains(`slider__cell`)) {
+          slide.classList.remove(`slider__cell`, `slider__cell--child`);
+        }
+      }
+    });
+
+    // wrappers
+    slidesWrappersCollection.forEach((wrapper) => {
+      if (mqlMobile.matches) {
+        if (wrapper.classList.contains(`slider__cell`)) {
+          wrapper.classList.remove(`slider__cell`, `slider__cell--child`);
+        }
+      }
+      if (mqlDesktop.matches) {
+        if (!wrapper.classList.contains(`slider__cell`)) {
+          wrapper.classList.add(`slider__cell`, `slider__cell--child`);
+        }
+      }
+    });
+  };
+
+  /**
+  * Control sliders' behaviour for different viewports
+  */
+  const matchMedia = () => {
+    transformInnerSlider();
+    changeSliderCell();
   };
 
   matchMedia();
