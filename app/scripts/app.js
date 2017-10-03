6f7c935a1521e1fa57bce2a7ebe3fd8af8aba292
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-03 22:47:53 +0300

    :fire: :sparkles: (app/blocks/slider, app/blocks/slide) Put styles back to flexbox layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 import svg4everybody from 'svg4everybody';
 
 // main-content
-import '../blocks/slider/slider.js';
+// import '../blocks/slider/slider.js';
 
 document.addEventListener(`DOMContentLoaded`, () => {
   svg4everybody();
