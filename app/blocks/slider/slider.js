17efd87d16b2d57248418f5931f3eacbbcbcaabb
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-07 15:44:22 +0300

    :sparkles: (app/blocks/slider) Add prev / next buttons

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -1,6 +1,17 @@
+import utils from '../../scripts/utils';
 import $ from 'jquery';
 import 'slick-carousel';
 
 export default window.addEventListener(`DOMContentLoaded`, () => {
-  $(`[data-js='slider--parent']`).slick();
+  // sliders
+  const parentSlider = document.querySelector(`[data-js='slider--parent']`);
+  // const childSlidersCollection = utils.$$(`[data-js='slider'].slider--child`);
+
+  // Slick' instances
+  const parentSlick = {
+    prevArrow: `.slider__prev`,
+    nextArrow: `.slider__next`
+  };
+
+  $(parentSlider).slick(parentSlick);
 });
