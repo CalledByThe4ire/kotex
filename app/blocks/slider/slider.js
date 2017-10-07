d077c4b7553b10fcd6dafc0a6f7a20ca1a3bae66
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-07 23:04:40 +0300

    :sparkles: (app/blocks/slider) Init inner slider

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -3,14 +3,63 @@ import $ from 'jquery';
 import 'slick-carousel';
 
 export default window.addEventListener(`DOMContentLoaded`, () => {
+  // media queries
+  const mqlMobile = window.matchMedia(`only screen and (max-width: 767px)`);
+  const mqlDesktop = window.matchMedia(`only screen and (min-width: 768px)`);
+
   // sliders
   const parentSlider = document.querySelector(`[data-js='slider--parent']`);
-  // const childSlidersCollection = utils.$$(`[data-js='slider'].slider--child`);
+  const childSlidersCollection = utils.$$(`[data-js='slider--child']`);
 
   // Slick' instances
   const parentSlick = {
+    infinite: true,
     arrows: true
   };
 
+  const childSlickForMobile = {
+    infinite: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
+
+  const childSlickForTabDesk = {
+    infinite: true,
+    arrows: false,
+    dots: true,
+    rows: 2,
+    slidesPerRow: 3
+  };
+
+  // Enable parent (outer) slider
   $(parentSlider).slick(parentSlick);
+
+  // Enable child (inners) sliders
+  childSlidersCollection.forEach(slider => {
+    $(slider).slick(childSlickForMobile);
+  });
+
+  /**
+  * Enable / disable inner slider for different viewports
+  */
+  const matchMedia = () => {
+    childSlidersCollection.forEach(slider => {
+      if (mqlMobile.matches) {
+        $(slider).slick(`unslick`);
+        $(slider).slick(childSlickForMobile);
+      }
+      if (mqlDesktop.matches) {
+        $(slider).slick(`unslick`);
+        $(slider).slick(childSlickForTabDesk);
+      }
+    });
+  };
+
+  matchMedia();
+
+  window.addEventListener(`resize`, () => {
+    matchMedia();
+  });
+
 });
